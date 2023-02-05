ecaa462f62f2154e48602be361bc31c87037c8ac
Munir Ali Mohammed <36868017+Mamsheikh@users.noreply.github.com>
2023-02-05 18:01:01 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import { Toaster } from 'react-hot-toast';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -11,7 +12,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         reverseOrder={false}
         toastOptions={{ duration: 2000 }}
       />
-      {/* <Analytics /> */}
+       <Analytics /> 
     </>
   );
 }
